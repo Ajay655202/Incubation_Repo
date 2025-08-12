001f2c2bc6e8a89bf51831ad727fe7389c4171fa
Ajay Radam <radam_ajay@epam.com>
2025-08-12 14:05:05 +0530

    Added one comment

--- LoggingAutomation/obj/Debug/net6.0/LoggingAutomation.AssemblyInfo.cs
+++ LoggingAutomation/obj/Debug/net6.0/LoggingAutomation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoggingAutomation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a9cab632a8bf2b8a101d03ed9c5d2a6b13dd4c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoggingAutomation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoggingAutomation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
